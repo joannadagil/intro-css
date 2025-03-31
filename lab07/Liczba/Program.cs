6a65bf72bb7b3e9b317f92f2db6f2cf40e6abc95
joannadagil <76077268+joannadagil@users.noreply.github.com>
2025-03-31 13:55:57 +0200

    Add files via upload

---
+++ lab07/Liczba/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Liczba
+{
+    class Program
+    {
+        static void Zapisz(int liczba, string path)
+        {
+            System.IO.StreamWriter plik_zapisu = new System.IO.StreamWriter(path,false);
+            plik_zapisu.WriteLine(liczba);
+            plik_zapisu.Close();
+        }
+
+        static int OdczytajLiczbę(string path)
+        {
+            System.IO.StreamReader reader = new System.IO.StreamReader(path);
+            int number = int.Parse(reader.ReadLine());
+            reader.Close();
+            return number;
+        }
+        static void Main(string[] args)
+        {
+            string path = "liczba.txt";
+            try
+            {
+                Zapisz(109404, path);
+            } 
+            catch (ObjectDisposedException e)
+            {
+                Console.WriteLine("Zapis się nie powiódł");
+            }
+            Console.WriteLine(OdczytajLiczbę(path));
+            Zapisz(109404, path);
+            Console.WriteLine(OdczytajLiczbę(path));
+
+        }
+    }
+}
