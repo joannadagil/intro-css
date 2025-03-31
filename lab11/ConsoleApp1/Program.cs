78f396a3ef33db1c4c3b9b2441d458479f0bbb65
joannadagil <76077268+joannadagil@users.noreply.github.com>
2025-03-31 13:59:48 +0200

    Add files via upload

---
+++ lab11/ConsoleApp1/Program.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    
+    public class Tree
+    {
+        public static int Comp(string a, string b)
+        {
+            int i = 0;
+            while (i < a.Length && i < b.Length)
+            {
+                if (a[i] < b[i]) return -1;
+                if (a[i] > b[i]) return 1;
+                i++;
+            }
+            if (a.Length == b.Length) return 0;
+            if (a.Length < b.Length) return -1;
+            return 1;
+        }
+
+        public static int Parse(string a)
+        {
+            int res = 0;
+            int i = 0;
+            while (i < a.Length)
+            {
+                res = res * 10 + a[i];
+                i++;
+            }
+            return res;
+        }
+
+        public class Node
+        {
+            public string name;
+            public int occur;
+            public Node left = null;
+            public Node right = null;
+
+            public Node(string name, int occur)
+            {
+                this.name = name;
+                this.occur = occur;
+            }
+        }
+
+        public Node root = null;
+
+        public void Dodaj(string osoba)
+        {
+            if (root == null)
+                root = new Node(osoba, 1);
+            else 
+            {
+                Node temp = root;
+                while (Comp(temp.name, osoba) != 0)
+                {
+                    if (Comp(temp.name, osoba) < 0)
+                    {
+                        if(temp.left == null)
+                            temp.left = new Node(osoba, 0);
+                        else                      
+                            temp = temp.left;
+                    }
+                    else
+                    {
+                        if (temp.right == null)
+                            temp.right = new Node(osoba, 0);
+                        else
+                            temp = temp.right;
+                    }
+                }
+                temp.occur++;
+            }
+            
+        }
+        public void Usun(string osoba)
+        {
+            Node temp = root;
+            while (Comp(temp.name, osoba) != 0)
+            {
+                if (Comp(temp.name, osoba) < 0)
+                    temp = temp.left;               
+                else              
+                    temp = temp.right;          
+            }
+            temp.occur--;
+        }
+
+        public Tree Odczyt(string nazwa)
+        {
+            Tree tree = new Tree();
+            StreamReader sr = new StreamReader(nazwa);
+            string line = sr.ReadLine();
+            while (line != null)
+            {
+                string name = line;
+                line = sr.ReadLine();
+                int occur = Parse(line);
+                for (int i = 0; i < occur; i++)
+                {
+                    tree.Dodaj(name);
+                }
+
+                line = sr.ReadLine();
+            }
+            sr.Close();
+            return tree;
+        }
+
+        void Zapisz_rec(StreamWriter sw, Tree.Node node)
+        {
+            if (node != null)
+            {
+                if (node.occur > 0)
+                {
+                    sw.WriteLine(node.name);
+                    sw.WriteLine(node.occur);
+                }
+                Zapisz_rec(sw, node.left);
+                Zapisz_rec(sw, node.right);
+            }
+        }
+        public void Zapisz(string nazwa)
+        {
+            StreamWriter sw = new StreamWriter(nazwa);
+            Zapisz_rec(sw, root);
+            sw.Close();
+        }
+        void Wyswietl_rec(Tree.Node node)
+        {
+            if (node != null)
+            {
+                if (node.occur > 0)
+                {
+                    Console.WriteLine(node.name);
+                    Console.WriteLine(node.occur);
+                }
+                Wyswietl_rec(node.left);
+                Wyswietl_rec(node.right);
+            }
+        }
+        public void Wyswietl()
+        {
+            Wyswietl_rec(root);
+            Console.WriteLine("");
+        }
+    }
+    class Program
+    {
+       
+
+        static void Main(string[] args)
+        {
+            Tree tree = new Tree();
+            tree.Dodaj("cccc");
+            tree.Dodaj("aaaa");
+            tree.Dodaj("aaaa");
+            tree.Dodaj("aaaa");
+            tree.Dodaj("bbbb");
+            tree.Dodaj("cccc");
+            tree.Dodaj("cccc");
+            tree.Wyswietl();
+            tree.Usun("bbbb");
+            tree.Wyswietl();
+            tree.Usun("aaaa");
+            tree.Wyswietl();
+            tree.Zapisz("plik.txt");
+            tree.Odczyt("plik.txt");
+            tree.Wyswietl();
+        }
+    }
+}
---
+++ lab13/ConsoleApp1/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        ushort Mod(short liczba)
+        {
+            return liczba > 0 ? (ushort)liczba : (ushort)(-liczba);
+        }
+        static ushort NajmniejszyDzielnik(short liczba, short dzielnik = 2)
+        {
+            if (liczba <= (short)(1)) return (ushort)1;
+            if (liczba % dzielnik == 0) return (ushort)dzielnik;
+            return NajmniejszyDzielnik(liczba, (short)(dzielnik + 1));
+            
+        }
+
+        static ushort IleSymboliBezwg(long liczba)
+        {
+            if (liczba / 10 > 0) return (ushort)(1 + IleSymboliBezwg(liczba / 10));
+            return (ushort)1;
+        }
+        static ushort ILeSymboli(long liczba)
+        {
+            if (liczba < 0) return (ushort)(1 + IleSymboliBezwg(-liczba));
+            return IleSymboliBezwg(liczba);
+        }
+
+        static ushort IleKiW(byte[,] tablica)
+        {
+            int res = 0;
+            int len0 = tablica.GetLength(0);
+            int len1 = tablica.GetLength(1);
+            for (int i = 0; i < len0; i++)
+            {
+                int temp = 0;
+                for (int j = 0; j < len1; j++)
+                {
+                    if (tablica[i, j] % 2 == 1) temp++;
+                }
+                if (temp > len0 / 2) res++;
+            }
+
+            for (int i = 0; i < len1; i++)
+            {
+                int temp = 0;
+                for (int j = 0; j < len0; j++)
+                {
+                    if (tablica[i, j] % 2 == 1) temp++;
+                }
+                if (temp > len1 / 2) res++;
+            }
+            return (ushort)res;
+        }
+
+        static ulong Suma(ulong liczba, ulong podst)
+        {
+            ulong res = 0;
+            while (liczba/podst > 0)
+            {
+                res += (liczba % podst);
+                liczba /= podst; 
+            }
+            res += (liczba % podst);
+            return res;
+        }
+
+        static ushort MaksSuma(ulong liczba)
+        {
+            ulong res = Suma(liczba, 2);
+            for (ulong i = 3; i <= 10; i++)
+            {
+                ulong temp = Suma(liczba, i);
+                res = temp < res ? temp : res;
+            }
+            return (ushort)res;
+        }
+
+        /*static bool CzyJest(char znak, string napis) { 
+           int [] alfab new int[char.MaxValue];
+        }*/
+
+        static string Roznorodna(string nazwa)
+        {
+            StreamReader sr = new StreamReader ("file.txt");
+            string line = sr.ReadLine();
+            ulong res_sym = 0;
+            string res_line = line;
+
+            while (line != null)
+            {
+                ulong sym = 0;
+                for (int i = 0; i < line.Length; i++)
+                {
+                    // sym = ??
+                    int j = 0;
+                    while (j < i && line[j] != line[i])
+                        j++;
+                    if (j != i) // jest takie samo przed
+                        sym++;
+                }
+                if (sym > res_sym)
+                {
+                    res_sym = sym;
+                    res_line = line;
+                }
+                line = sr.ReadLine();
+            }
+            
+            sr.Close();
+            return res_line;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("NajmniejszyDzielnik(21) = " + NajmniejszyDzielnik(21));
+
+            Console.WriteLine("ILeSymboli(-21) = " + ILeSymboli(-21));
+
+            byte[,] tab = { { 1, 0, 1 }, { 1, 0, 0 }, { 0, 0, 0 } };
+            Console.WriteLine("ILeKiW(tab1) = " + IleKiW(tab));
+            byte[,] tab2 = { { 1, 0, 1, 0 }, { 1, 0, 0, 0 }, { 0, 0, 0, 0 }, { 0, 0, 0, 0 } };
+            Console.WriteLine("ILeKiW(tab2) = " + IleKiW(tab2));
+
+            Console.WriteLine("MaksSuma(99) = " + MaksSuma(99));
+        }
+    }
+}
