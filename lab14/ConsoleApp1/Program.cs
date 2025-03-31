d07fd12d9e9753335d5155f0c18a725850277d1c
joannadagil <76077268+joannadagil@users.noreply.github.com>
2025-03-31 14:00:59 +0200

    Add files via upload

---
+++ lab14/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        static int Spolgloski(string napis)
+        {
+            int suma = 0;
+            string spolgloski = "BbCcDdFfGgHhJjKkLlMmNnPpRrSsTtWwXxZz";
+            for (int i = 0; i < napis.Length; i++)
+            {
+                char n = napis[i];
+                
+                for (int j = 0; j < spolgloski.Length; j++)
+                {
+                    if (n == spolgloski[j])
+                        suma++;
+                }
+            }
+            return suma;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Spolgloski("Hello World!"));
+        }
+    }
+}
